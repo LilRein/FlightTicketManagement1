0029d4429e93fad994c9fb50f469443c4a51642f
LilRein <153301586+LilRein@users.noreply.github.com>
2024-04-27 21:59:00 +0700

    Add project files.

---
+++ FlightTicketManagement/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FlightTicketManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
