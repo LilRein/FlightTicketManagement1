937061ca20351eacffe204984fbeadce513452bb
Shieng <22521312@gm.uit.edu.vn>
2024-05-04 18:48:41 +0700

    UpdateAddFlightView

--- FlightTicketManagement/ViewModel/AddFlightVM.cs
+++ FlightTicketManagement/ViewModel/AddFlightVM.cs
@@ -16,6 +16,9 @@ namespace FlightTicketManagement.ViewModel
         public ICommand AddCommand { get; set; }
         public ICommand CloseAACM { get; set; }
 
+        public ICommand ConfirmCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
         public AddFlightVM() 
         {
             FlightItemList = new ObservableCollection<string>
@@ -27,6 +30,8 @@ namespace FlightTicketManagement.ViewModel
 
             };
             CloseAACM = new RelayCommand<AddFlight>((p) => true, (p) => _CloseAACM(p));
+            ConfirmCommand = new RelayCommand<AddFlight>((p) => true, (p) => _ConfirmCommand(p));
+            CancelCommand = new RelayCommand<AddFlight>((p) => true, (p) => _CancelCommand(p));
         }
 
         private ObservableCollection<string> _flightItemList;
@@ -49,6 +54,32 @@ namespace FlightTicketManagement.ViewModel
             }
         }
 
-       
+        private void _ConfirmCommand(AddFlight paramater)
+        {
+            if (paramater.FlightID.Text == "" || string.IsNullOrEmpty(paramater.StartListAirport.Text) || 
+                string.IsNullOrEmpty(paramater.EndListAirport.Text) || 
+                string.IsNullOrEmpty(paramater.FlightTime.Text)||
+                paramater.Seat1.Text == "" || paramater.Seat2.Text == "")
+            {
+                MessageBox.Show("Có vẻ bạn thiếu thông tin!!", "Notification", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            MessageBoxResult addFliNoti = System.Windows.MessageBox.Show("Bạn muốn thêm chuyến bay ?", "Notification", MessageBoxButton.YesNo);
+            if (addFliNoti == MessageBoxResult.Yes)
+            {
+                //Xử lí logic khi nhấn Yes
+                MessageBox.Show("Test thôi không có gì đâu");
+            }
+        }
+
+        private void _CancelCommand(AddFlight paramater)
+        {
+            paramater.FlightID.Clear();
+            paramater.StartListAirport.SelectedItem = null;
+            paramater.EndListAirport.SelectedItem = null;
+            paramater.FlightTime.Text = string.Empty;
+            paramater.Seat1.Clear();
+            paramater.Seat2.Clear();
+        }
     }
 }
