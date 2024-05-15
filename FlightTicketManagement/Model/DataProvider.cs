9392e44b698353725a6f557125e28f479b53f4b9
nhatle10 <canhnhat922017@gmail.com>
2024-05-15 19:13:41 +0700

    update datap

--- FlightTicketManagement/Model/DataProvider.cs
+++ FlightTicketManagement/Model/DataProvider.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryManagement.Model
+namespace FlightTicketManagement.Model
 {
     public class DataProvider
     {
